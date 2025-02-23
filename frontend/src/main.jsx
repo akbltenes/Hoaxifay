13de804d7a397cd11d0838253051e22b2ede86ab
Enes PC <akbltenes638@gmail.com>
2025-02-24 02:47:56 +0300

    Frontend Dosyaları

---
+++ frontend/src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+//import './index.css'
+
+//import App from './App.jsx'
+import { SignUp } from "./pages/SignUp/index.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <SignUp />
+  </StrictMode>
+);
---
+++ frontend/src/pages/SignUp/index.jsx
@@ -0,0 +1,138 @@
+import { useEffect, useState } from "react";
+import { signUp } from "./api";
+
+export function SignUp() {
+  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [passwordRepeat, setPasswordRepeat] = useState();
+  const [apiProgress, setApiProgress] = useState(false);
+  const [successMessage, setSuccessMessage] = useState();
+  const [errors, setErrors] = useState({});
+  const [generalError, setGeneralError] = useState();
+  useEffect(() => {
+    setErrors({});
+  }, [username]);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setSuccessMessage();
+    setGeneralError();
+    setApiProgress(true);
+    try {
+      const response = await signUp({
+        username,
+        email,
+        password,
+      });
+      setSuccessMessage(response.data.message);
+    } catch (axiosError) {
+      if (
+        axiosError.response?.data &&
+        axiosError.response.data.status === 400
+      ) {
+        setErrors(axiosError.response.data.validationErrors);
+      } else {
+        setGeneralError("Bir hata oluştu.Lütfen tekrar deneyin");
+      }
+    } finally {
+      setApiProgress(false);
+    }
+  };
+
+  return (
+    <>
+      <div className="container">
+        <div className="col-lg-6 offset-lg-3 col-sm-8 offset-sm-2">
+          <form className="card " onSubmit={onSubmit}>
+            <div
+              className="text-center card-header bg-dark text-white "
+              style={{ width: "100%" }}
+            >
+              <h1>Sign Up</h1>
+            </div>
+            <div className="card-body ">
+              <div className="mb-2">
+                <label htmlFor="username" className="form-label ">
+                  Username:{" "}
+                </label>
+                <input
+                  id="username"
+                  className={
+                    errors.username
+                      ? "form-control is-invalid "
+                      : "form-control"
+                  }
+                  value={username}
+                  onChange={(event) => {
+                    setUsername(event.target.value);
+                  }}
+                />
+              </div>
+              <div className="invalid-feedback">{errors.username}</div>
+              <div className="mb-2">
+                <label htmlFor="email" className="form-label">
+                  E-mail:{" "}
+                </label>
+                <input
+                  id="email"
+                  className="form-control"
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
+                />
+              </div>
+              <div className="mb-2">
+                <label htmlFor="password" className="form-label ">
+                  Password:{" "}
+                </label>
+                <input
+                  id="password"
+                  type="password"
+                  className="form-control"
+                  value={password}
+                  onChange={(event) => setPassword(event.target.value)}
+                />
+              </div>
+              <div className="mb-2">
+                <label htmlFor="passwordRepeat" className="form-label ">
+                  Password Repeat:{" "}
+                </label>
+                <input
+                  id="passwordRepeat"
+                  type="password"
+                  className="form-control"
+                  onChange={(event) => setPasswordRepeat(event.target.value)}
+                />
+              </div>
+
+              {/*backende gonder*/}
+              {successMessage && (
+                <div className="alert alert-success">{successMessage}</div>
+              )}
+              {generalError && (
+                <div className="alert alert-danger">{generalError}</div>
+              )}
+              <div className="text-center">
+                <button
+                  className="btn btn-success"
+                  type="submit"
+                  disabled={apiProgress || password !== passwordRepeat}
+                >
+                  {apiProgress && (
+                    <span
+                      className="spinner-border spinner-border-sm"
+                      aria-hidden="true"
+                    ></span>
+                  )}
+                  <span className="visually-hidden" role="status">
+                    Loading...
+                  </span>
+                  Sign Up
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
